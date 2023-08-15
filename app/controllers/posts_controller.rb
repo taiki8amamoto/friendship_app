f676e5854d41b816afae9bb7d96d06d5cccffcef
taiki yamamoto <ultra0115.20@gmail.com>
2023-08-15 10:42:14 +0900

    new,index,show画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,58 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "新たに投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+  
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました！"
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:image, :image_cache, :content)
+  end
+
 end
